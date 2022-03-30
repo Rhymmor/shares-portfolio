b959bf2a31c7abbff9bfb9a9825efbfa1be6121b
Anatoly Belonog <anatolybelonog@gmail.com>
2022-03-31 00:52:13 +0700

    portfolio as yaml

---
+++ src/data/portfolio.py
@@ -0,0 +1,10 @@
+from collections import OrderedDict
+import yaml
+from src.domain.distribution import FundsDistribution, SharesDistribution
+
+def import_portfolio(file_path: str) -> SharesDistribution:
+    with open(file_path, 'r') as f:
+        funds = OrderedDict(yaml.safe_load(f.read()))
+        shares = FundsDistribution(funds=funds)
+
+        return SharesDistribution(shares=shares)
